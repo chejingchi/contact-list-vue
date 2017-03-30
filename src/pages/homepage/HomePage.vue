d511e979e6904e479d1225f8e30de03e1b8ac565
michaelChe <1020934146@qq.com>
2017-03-30 14:44:27 +0800

    add first component

---
+++ src/pages/homepage/HomePage.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="homePage">
+  {{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'homePage',
+  data () {
+    return {
+      msg: 'welcome to my contact-list'
+    }
+  }
+}
+</script>
+
+<style scoped lang="less" rel="stylesheet/less">
+
+
+
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
