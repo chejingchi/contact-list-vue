314411cce5db96f20e87d100f65201b359968257
michaelChe <1020934146@qq.com>
2017-04-06 13:58:52 +0800

    change proxytable name

--- config/index.js
+++ config/index.js
@@ -28,7 +28,7 @@ module.exports = {
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
-      '/gym': 'http://localhost:8080'
+      '/contact-list-admin': 'http://localhost:8080'
     },
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
